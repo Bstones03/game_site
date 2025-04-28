e1fd65561ada427ba958ca8a30cd1481a808ed75
Bstones <brennenstones@gmail.com>
2025-04-28 15:34:07 -0500

    fixed login errors

--- games/views.py
+++ games/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from accounts.models import UserProfile
 
 def play_game(request):
-    profile = UserProfile.objects.get(user=request.user)
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
     return render(request, 'games/play.html', {'profile': profile})
