cd56bc41e8e2d506cb822a16927d16ebd0f56dca
Bstones <brennenstones@gmail.com>
2025-05-05 14:34:10 -0500

    added reroute from base url to the login page

--- game_site/urls.py
+++ game_site/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # for login/logout
